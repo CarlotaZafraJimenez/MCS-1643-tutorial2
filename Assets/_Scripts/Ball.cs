9232a323dfb78ae15f02b9043202174b1bbf3931
CarlotaZafraJimenez <czafrajim@ltu.edu>
2025-09-30 20:02:56 +0200

    .

--- Assets/_Scripts/Ball.cs
+++ Assets/_Scripts/Ball.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class Ball : MonoBehaviour
+    public class Ball : MonoBehaviour
 {
     public AudioClip bounceSound;
     public float speed = 36.0f;
